0d03c804a9d0008f9e71029970341f610db4fd42
Igor V. Teixeira <igorvitort@gmail.com>
2025-09-13 15:14:36 -0300

    Change port

--- api/app.py
+++ api/app.py
@@ -33,4 +33,4 @@ def health_check():
     return {"status": "ok"}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5051, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
